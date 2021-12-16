265523c1bc7007602fb23f3f911e827406618013
paulotijero <paulotijero@gmail.com>
2021-12-16 08:05:53 -0500

    ADD: Forward movements

--- movement.rb
+++ movement.rb
@@ -1,3 +1,36 @@
-def movement(world)
-  return 'Hello '+ world
+def movement(rectangular_world, robots)
+  final_positions = []
+  lost_trail = []
+  
+  robots.each do |robot|
+    position = robot[:initial]
+
+    robot[:movements].each do |movement|
+      case movement
+      when 'F'
+        case position[:direction]
+        when 'N'
+          position[:y] = position[:y] + 1
+        when 'E'
+          position[:x] = position[:x] + 1
+        when 'S'
+          position[:y] = position[:y] - 1
+        when 'W'
+          position[:x] = position[:x] - 1
+        else
+          'ilegal direction'
+        end
+      when 'R'
+        position = [1,2,'right']
+      when 'L'
+        position = [1,2,'left']
+      else
+        "ilegal movement"
+      end
+    end
+
+    final_positions << position
+  end
+
+  final_positions
 end
--- spec/movement_spec.rb
+++ spec/movement_spec.rb
@@ -2,12 +2,68 @@ require_relative '../movement'
 
 describe 'Movement' do
 
-  it "Test to include Hello world" do
-    expect(movement('World')).to include('Hello World')
+  describe "Forward tests" do
+    world = {x: 5, y: 5}
+    
+    it "advance to position [0, 4, 'N']" do
+      robots = [
+        {
+          initial: {x: 0, y: 0, direction: 'N'},
+          movements: ['F','F','F','F']
+        }
+      ]
+      
+      expect(movement(world, robots)).to eq([{x: 0, y: 4, direction: "N"}])
+    end
+
+    it "advance to position [4, 0, 'E']" do
+      robots = [
+        {
+          initial: {x: 0, y: 0, direction: 'E'},
+          movements: ['F','F','F','F']
+        }
+      ]
+      
+      expect(movement(world, robots)).to eq([{x: 4, y: 0, direction: "E"}])
+    end
+
+    it "advance to position [5, 1, 'S']" do
+      robots = [
+        {
+          initial: {x: 5, y: 5, direction: 'S'},
+          movements: ['F','F','F','F']
+        }
+      ]
+      
+      expect(movement(world, robots)).to eq([{x: 5, y: 1, direction: "S"}])
+    end
+
+    it "advance to position [5 , 5, 'W']" do
+      robots = [
+        {
+          initial: {x: 5, y: 5, direction: 'W'},
+          movements: ['F','F','F','F']
+        }
+      ]
+      
+      expect(movement(world, robots)).to eq([{x: 1, y: 5, direction: "W"}])
+    end
+
+    it "return 2 final positions" do
+      robots = [
+        {
+          initial: {x: 0, y: 0, direction: 'N'},
+          movements: ['F','F','F','F']
+        },
+        {
+          initial: {x: 0, y: 0, direction: 'E'},
+          movements: ['F','F','F','F']
+        },
+      ]
+      
+      expect(movement(world, robots).count).to eq(2)
+    end
+    
   end
 
-  it "Test not equal Hello world" do
-    expect(movement('World')).not_to eq('Hello')
-  end
-  
 end
